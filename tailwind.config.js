41ec1842648036460135571c8f129e90fb27a25b
allysonkaye <allysonkayetimpug06@gmail.com>
2023-06-02 23:53:57 +0800

    change the directory in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/*.{html,js}"],
+  content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
   },
